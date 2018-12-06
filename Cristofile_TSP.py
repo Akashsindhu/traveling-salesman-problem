f4a833b2a9a310a6bdb94abd78139b72785dfcb9
manchavz <manu36chavez@gmail.com>
2018-12-05 20:27:48 -0800

    Added GenerateGraph() and sketchGraph()

--- Cristofile_TSP.py
+++ Cristofile_TSP.py
@@ -31,3 +31,42 @@ def union(self, order, x, y):
 
 def christofedes(G, x):
     graph=nx.Graph()
+
+def GenerateGraph():
+    # Instantiate an empty Graph with no Vertices(Nodes) and Edges
+    G = net.Graph()
+    # Assign a variable the contents of the Text File
+    graph_specs = open('graph_5vert.txt')
+    # typecast data in Text_File from string into int
+    file_oneline = int(graph_specs.readline())
+    # list if edge weights matrix
+    weightMatrix = []
+    # variable for each line from the text file (string)
+    file_oneline2 = graph_specs.readline() # string value
+    # val = each string line in text file
+    for val in range(file_oneline):
+        # typecast indivual string edge_weight values into integers
+        oneline_file_values_list = map(int,file_oneline2.split())
+        # append edge_weight values to the Matrix rows
+        weightMatrix.append(oneline_file_values_list)
+
+    # Adds edges along with their weights to the graph
+    for row in range(file_oneline):
+        for column in range(file_oneline)[row:]:
+            # if the edge_weight between two nodes is greater than 0
+            if weightMatrix[row][column] > 0:
+                # add the edge_weight to [row]x[column] described path
+                G.add_edge(row, column, distance = weightMatrix[row][column])
+    return G # return a Graph
+
+def sketchGraph(G, color):
+    # returns a Dictionary of positions keyed by node
+    node_position = net.spring_layout(G)
+    # draws the graph using matplotlib
+    net.draw(G,node_position,with_labels = True, edge_color = color)
+    # returns a Dictionary of Attributes keyed by edge. Multi(di)graph, Keys are 3-tuples(u,v,key)
+    edge_labels = net.get_edge_attributes(G,'distance')
+    # returns a Dictionary of labels keyed on the Edges
+    net.draw_networkx_edge_labels(G,node_position,edge_labels = edge_labels)
+
+    return node_position
